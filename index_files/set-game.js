6884c5568a7cf817fc610c23ee153fb5286190d2
Richard Liu <rtliu@mit.edu>
2020-05-19 15:45:54 -0400

    improve protocol choice

--- index_files/set-game.js
+++ index_files/set-game.js
@@ -179,7 +179,7 @@ function setGame() {
       playButton.disabled = true;
     }
     setGame.server = server;
-    if (setGame.server === 'localhost:8080') {
+    if (setGame.server.endsWith(':8080') { // TODO improve this?
       setGame.protocol = 'http://';
     } else {
       setGame.protocol = 'https://';
